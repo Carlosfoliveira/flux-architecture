99a23054c57dea7505fe70baf4dd5f8b7ed8d64d
carlosfoliveira <carlos.fe.oliveira@gmail.com>
2020-08-24 13:42:35 -0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2020: true,
+    },
+    extends: [
+      'plugin:react/recommended',
+      'airbnb',
+    ],
+    parser: 'babel-eslint',
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 11,
+      sourceType: 'module',
+    },
+    plugins: [
+      'react',
+    ],
+    rules: {
+      'react/jsx-filename-extension': [
+        'warn',
+        { extensions: ['.jsx', '.js'] }
+      ],
+      'import/prefer-default-export': 'off'
+    },
+  };
+  
